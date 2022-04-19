2e02579cb5650670fff2cccd5e7af151760726c6
unknown <43129513+techtobit@users.noreply.github.com>
2022-04-19 23:58:43 +0600

    Done

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 
 # Project Link #
 [github](https://github.com/programming-hero-web-course-4/independent-service-provider-techtobit) 
+[Netlyfi](https://startling-gelato-1988ff.netlify.app) 
+[Firebase](https://edu-co-cc85b.firebaseapp.com/) 
+
+firebase e problem korce. oank try korce.
 
 # Futures Add #
 * Create a Site
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -3,14 +3,19 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
 // Import the functions you need from the SDKs you need
 // Your web app's Firebase configuration
+// Import the functions you need from the SDKs you need
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyCisaj9GY0KjzdaqsrHkgiF2bqmHouhzZg",
   authDomain: "edu-co-cc85b.firebaseapp.com",
   projectId: "edu-co-cc85b",
   storageBucket: "edu-co-cc85b.appspot.com",
   messagingSenderId: "1065122388352",
-  appId: "1:1065122388352:web:1ad282a810121bb80bc965"
+  appId: "1:1065122388352:web:bb54812a27b89d230bc965"
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
